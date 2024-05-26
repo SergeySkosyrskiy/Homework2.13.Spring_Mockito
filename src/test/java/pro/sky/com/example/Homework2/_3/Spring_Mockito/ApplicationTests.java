888a12c04eaa3254102e4ebd5d6be29e677aba6e
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-05-26 10:56:33 +0800

    Homework

---
+++ src/test/java/pro/sky/com/example/Homework2/_3/Spring_Mockito/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.com.example.Homework2._3.Spring_Mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
